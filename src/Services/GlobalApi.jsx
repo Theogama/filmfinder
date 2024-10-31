2f2eb83b99c54eb6da99b39118741c734279a14c
unknown <Sandile>
2024-10-31 22:21:49 +0200

    Update Global Api

--- src/Services/GlobalApi.jsx
+++ src/Services/GlobalApi.jsx
@@ -1,14 +1,14 @@
 import axios from "axios";
 
 const movieBaseUrl="https://api.themoviedb.org/3"
-const api_key="1d60b0f2d1f9bec7f3ec40d15335c3be"
-const movieByGenreBaseURL='https://api.themoviedb.org/3/discover/movie?api_key='+api_key;
+const API_KEY=api_key
+const movieByGenreBaseURL='https://api.themoviedb.org/3/discover/movie?api_key='+API_KEY;
 
 
 //https://api.themoviedb.org/3/trending/all/day?api_key=1d60b0f2d1f9bec7f3ec40d15335c3be 
 
 const getTrendingVideos=axios.get(movieBaseUrl+
-    "/trending/all/day?api_key="+api_key);
+    "/trending/all/day?api_key="+API_KEY);
     const getMovieByGenreId=(id)=>
     axios.get(movieByGenreBaseURL+"&with_genres="+id)
 
