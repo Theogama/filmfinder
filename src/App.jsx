561b8fa225c15e67a5312f7d4414d11f3e8829a9
unknown <Sandile>
2024-10-31 20:25:08 +0200

    update-browserslist-db

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 import Header from './Componets/Header'
 import Slider from './Componets/Slider'
@@ -10,12 +8,12 @@ import GenreMovieList from './Componets/GenreMovieList'
 
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
     <div className=''>
       <Header />
-      <Slider/>
+      <Slider />
       <ProductionHouse />
       <GenreMovieList />
     </div>
